309a1c35c022d696e8ea1ef5c9aff88a1e748503
shei2 <jeydeleon197@gmail.com>
2024-08-17 16:33:33 -0300

    boton arreglado

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
 document.getElementById('div').addEventListener('click',function(){
     alert('Hola! Soy el div!');
 })
+
+document.getElementById('boton').addEventListener('click',function(event) {
+   
+   
+    event.stopPropagation();
+    alert('Hola!');
+   });
